6362bece3370beec0fecd2364fe5ca594502e0fc
JuanaVillaba <137566713+JuanaVillaba@users.noreply.github.com>
2025-04-27 23:57:31 -0300

    Ejercicio 1 y 2

---
+++ Tarea_4/Ejercicio1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Ejercicio1 {
+ class Program
+    {
+        public static void Main(string[] args)
+        {
+            Persona p1 = new Persona("Boomer", 5, 45152666);
+            Persona p2 = new Persona("Lola");
+            Persona p3 = new Persona();
+            p1.MostrarDatos(p1.nombre, p1.GetEdad(), p1.dni);
+            p2.MostrarDatos(p2.nombre, p2.GetEdad(), p2.dni);
+            p3.MostrarDatos(p3.nombre, p3.GetEdad(), p3.dni);
+            p1.setEdad(p1.nombre, p1.GetEdad());
+            p2.setEdad(p2.nombre, p2.GetEdad());
+            p3.setEdad(p3.nombre, p3.GetEdad());
+            p1.MostrarDatos(p1.nombre, p1.GetEdad(), p1.dni);
+            p2.MostrarDatos(p2.nombre, p2.GetEdad(), p2.dni);
+            p3.MostrarDatos(p3.nombre, p3.GetEdad(), p3.dni);
+        }
+ }
+ class Persona
+    {
+        public string nombre;
+        private int edad;
+        public int dni;
+
+        public Persona(string nombre, int edad, int dni)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.dni = dni;
+        }
+        public Persona(string nombre)
+        {
+            this.nombre = nombre;
+            this.edad = 1;
+            this.dni = 11111111;
+        }
+        public Persona()
+        {
+            this.nombre = "Sin nombre";
+            this.edad = 0;
+            this.dni = 00000000;
+        }
+        public void MostrarDatos(string nombre, int edad, int dni)
+        {
+            Console.WriteLine($"{nombre} {edad} {dni}");
+        }
+        public int GetEdad()
+        {
+            return edad;
+        }
+        public int setEdad(string nombre, int edad)
+        {
+            int nueva_edad;
+            Console.WriteLine($"Ingrese la nueva edad de {nombre}:");
+            nueva_edad = int.Parse(Console.ReadLine());
+            return this.edad = nueva_edad;
+        }
+ }
+}
---
+++ Tarea_4/Ejercicio_2/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Ejercicio2
+{
+    class CALCULO
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, resultado, buscar;
+            double A, B, C, RESULTADO;
+            bool Booleano;
+            Calculadora calculadora = new Calculadora();
+
+            Console.WriteLine("Ingrese numero para sumar: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese numero para sumar: ");
+            b = int.Parse(Console.ReadLine());
+ 
+            resultado = calculadora.Sumar(a, b);
+            Console.WriteLine($"El resultado es: {resultado}");
+
+            Console.WriteLine("Ingrese numero para sumar: ");
+            A = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese numero para sumar: ");
+            B = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese numero para sumar: ");
+            C = double.Parse(Console.ReadLine());
+
+            RESULTADO = calculadora.Sumar3(A, B, C);
+            Console.WriteLine($"El resultado es: {RESULTADO}");
+
+            calculadora.Sumar_Mensajes();
+            Console.WriteLine("Ingrese un numero para saber si es par o impar: ");
+            buscar = int.Parse(Console.ReadLine());
+            Booleano = calculadora.Par_Impar(buscar);
+            Console.WriteLine($"El numero {buscar} es {Booleano}");
+            Console.WriteLine("FIN");
+            
+        }
+    }
+    class Calculadora
+    {
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+        public double Sumar3(double A, double B, double C)
+        {
+            return A + B + C;
+        }
+        public void Sumar_Mensajes() {
+            string mensaje;
+            int numero_A, numero_B, resultado;
+            Console.WriteLine("Ingrese mensaje: ");
+            mensaje = Console.ReadLine();
+            Console.WriteLine("Ingrese numero: ");
+            numero_A=int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese numero: ");
+            numero_B = int.Parse(Console.ReadLine());
+            resultado =numero_A +numero_B;
+            Console.WriteLine($"El mensaje es: {mensaje}");
+            Console.WriteLine($"La suma es {resultado}");
+        }
+        public bool Par_Impar(int buscar)
+        {
+            bool respuesta;
+            if (buscar % 2 == 0) {
+                return respuesta =true;
+            }
+            else
+            {
+                return respuesta = false;
+            }
+        }
+    }
+}
